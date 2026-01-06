9b0d799fe0e712b95bc4c0bfec5ceab7565a0513
Lndr16 <leandrelemoine.com@gmail.com>
2026-01-06 12:05:58 +0100

    Add comments to serveur_tcpip.c for clarity
    Added comments for better understanding of server code.

--- serveur_tcpip.c
+++ serveur_tcpip.c
@@ -15,6 +15,7 @@
 #define SIZE 100
 
 int main(int argc, char *argv[]){
+    /*Déclarations des variables du serveur*/
     int sockfd, newsockfd;
     struct sockaddr_in server_addr, client_addr;
     socklen_t adr_len;
@@ -22,14 +23,17 @@ int main(int argc, char *argv[]){
     int n;
     int opt = 1;
 
+    /*Création du socket*/
     sockfd = socket(PF_INET,SOCK_STREAM, 0);
     if(sockfd < 0) {
         perror("Erreur Socket");
         exit(1);
     }
 
+    /*Permet au socket d'être lié à une adresse déjà utilisée*/
     setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
+    /*Structure d'adresse locale du serveur*/
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = PF_INET; 
     server_addr.sin_port = htons(PORT);
@@ -47,6 +51,7 @@ int main(int argc, char *argv[]){
 
     printf("Serveur en attente de connexion sur le port %d...\n", PORT);
 
+    /*Boucle de connexion aux clients*/
     while(1){
 
         adr_len = sizeof(client_addr);
@@ -57,6 +62,7 @@ int main(int argc, char *argv[]){
 
         printf("Client connecté.\n");
 
+        /*Boucle d'échange avec le client*/
         while(1){
             memset(buffer, 0, SIZE);
             n = read(newsockfd, buffer, SIZE - 1);
@@ -69,14 +75,15 @@ int main(int argc, char *argv[]){
                 break;
             }
             printf("Message reçu: %s\n", buffer);
+
+            /*Renvoie au client le message reçu*/
             write(newsockfd, buffer, n);
         }
         
+        /*Fermeture de connexion avec le client en attente d'un nouveau*/
         close(newsockfd);
-        break;
     }
     close(sockfd);
-    printf("Serveur arrêté.\n");
     return 0;
 
 }
